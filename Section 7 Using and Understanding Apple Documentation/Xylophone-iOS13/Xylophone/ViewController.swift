7bc23b7113611e36cb3bafb0cc1417f04b0a56ce
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-10-02 13:23:39 +0300

    readd sections

---
+++ Section 7 Using and Understanding Apple Documentation/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+  
+  var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    
+  @IBAction func redKeyPressed(_ sender: UIButton) {
+    playSound(buttonTitle: sender.currentTitle!)
+    
+  }
+  
+  func playSound(buttonTitle: String) {
+      let url = Bundle.main.url(forResource: "\(buttonTitle)", withExtension: "wav")
+      player = try! AVAudioPlayer(contentsOf: url!)
+      player.play()
+              
+  }
+}
+
