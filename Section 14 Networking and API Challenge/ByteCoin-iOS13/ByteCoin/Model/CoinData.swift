0801da180e369a147c0e904ee6f320123f2a3ed6
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-11-12 23:11:34 +0300

    14th section completed

---
+++ Section 14 Networking and API Challenge/ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by malakoipechyva on 11/12/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+  let rate: Double
+}
