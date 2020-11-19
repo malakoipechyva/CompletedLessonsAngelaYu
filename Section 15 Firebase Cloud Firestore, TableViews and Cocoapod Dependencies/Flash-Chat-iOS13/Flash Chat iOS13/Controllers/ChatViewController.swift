791f9c2676f440a5d60306651ac064432de7795f
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-11-19 11:03:15 +0300

    add auth and configure segues

--- Section 15 Firebase Cloud Firestore, TableViews and Cocoapod Dependencies/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Section 15 Firebase Cloud Firestore, TableViews and Cocoapod Dependencies/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
-
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var messageTextfield: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+  
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var messageTextfield: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationItem.hidesBackButton = true
+    title = "⚡️FlashChat"
+  }
+  
+  @IBAction func sendPressed(_ sender: UIButton) {
+  }
+  
+  @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+    let firebaseAuth = Auth.auth()
+    do {
+      try firebaseAuth.signOut()
+      navigationController?.popViewController(animated: true)
+    } catch let signOutError as NSError {
+      print ("Error signing out: %@", signOutError)
     }
     
-    @IBAction func sendPressed(_ sender: UIButton) {
-    }
-    
-
+  }
+  
 }
