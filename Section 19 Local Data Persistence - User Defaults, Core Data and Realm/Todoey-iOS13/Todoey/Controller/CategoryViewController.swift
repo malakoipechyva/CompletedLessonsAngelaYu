405d116eedbfac85df7311c68849cbd723961654
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-12-16 17:32:18 +0300

    add chameleonFramework

--- Section 19 Local Data Persistence - User Defaults, Core Data and Realm/Todoey-iOS13/Todoey/Controller/CategoryViewController.swift
+++ Section 19 Local Data Persistence - User Defaults, Core Data and Realm/Todoey-iOS13/Todoey/Controller/CategoryViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class CategoryViewController: SwipeTableViewController {
   
@@ -17,6 +18,7 @@ class CategoryViewController: SwipeTableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      tableView.separatorStyle = .none
       
       loadCategories()
     }
@@ -30,6 +32,7 @@ class CategoryViewController: SwipeTableViewController {
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = super.tableView(tableView, cellForRowAt: indexPath)
     cell.textLabel?.text = categories?[indexPath.row].name ?? "no categories added yet"
+    cell.backgroundColor = UIColor(hexString: categories?[indexPath.row].color ?? "1D9BF6")
     
     return cell
   }
@@ -55,6 +58,7 @@ class CategoryViewController: SwipeTableViewController {
     let action = UIAlertAction(title: "add", style: .default) { (action) in
       let newCategory = Category()
       newCategory.name = texField.text!
+      newCategory.color = UIColor.randomFlat().hexValue()
 
       self.save(category: newCategory)
     }
